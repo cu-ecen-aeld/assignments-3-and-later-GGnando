ad11b386877c1781ed808da740dc3e9d1210ba06
vboxuser <vboxuser@ubuntu20.myguest.virtualbox.org>
2025-02-14 23:08:45 -0700

    Added exec calls to systemcalls.c

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -16,8 +16,9 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
-    return true;
+    // system() returns -1 if call fails. return value will depend on what
+    // the return value from cmd is. assume 0 is success
+    return system(cmd) == 0;
 }
 
 /**
@@ -36,6 +37,7 @@ bool do_system(const char *cmd)
 
 bool do_exec(int count, ...)
 {
+    bool ret = false;
     va_list args;
     va_start(args, count);
     char * command[count+1];
@@ -45,9 +47,6 @@ bool do_exec(int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
 
 /*
  * TODO:
@@ -58,10 +57,42 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+    int pid = fork();
+
+    if(pid == -1)
+    {
+        perror("error invoking fork()");
+        ret = false;
+    }
+    else
+    {
+        // child
+        if(pid == 0)
+        {
+            execv(command[0], command);
+            // only set happens if execv returns, this means it failed
+            perror("execv() failure");
+            exit(-1);
+        }
+        // parent
+        else
+        {
+            int status;
+            if(wait(&status) != -1)
+            {
+                // true if child terminated normally and had an exit status of 0 (success)
+                ret = WIFEXITED(status) && (WEXITSTATUS(status) == 0);
+            }
+            else
+            {
+                ret = false;
+            }
+        }
+    }
 
     va_end(args);
-
-    return true;
+    printf("ret = %d\n",ret);
+    return ret;
 }
 
 /**
@@ -71,6 +102,7 @@ bool do_exec(int count, ...)
 */
 bool do_exec_redirect(const char *outputfile, int count, ...)
 {
+    bool ret = false;
     va_list args;
     va_start(args, count);
     char * command[count+1];
@@ -80,11 +112,6 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
         command[i] = va_arg(args, char *);
     }
     command[count] = NULL;
-    // this line is to avoid a compile warning before your implementation is complete
-    // and may be removed
-    command[count] = command[count];
-
-
 /*
  * TODO
  *   Call execv, but first using https://stackoverflow.com/a/13784315/1446624 as a refernce,
@@ -92,8 +119,51 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+    int pid = fork();
+
+    if(pid == -1)
+    {
+        perror("error invoking fork()");
+        ret = false;
+    }
+    else
+    {
+        // child
+        if(pid == 0)
+        {
+            int fd = open(outputfile, O_RDWR|O_TRUNC|O_CREAT, 0644);
+            if (dup2(fd, STDOUT_FILENO) < 0)
+            { 
+                perror("dup2 failure");
+                ret = false;
+            }
+            else
+            {
+                execv(command[0], command);
+                // only set if execv return, this means it failed
+                perror("execv() failure");
+                exit(-1);
+            }
+        }
+        // parent
+        else
+        {
+            int status;
+            if(wait(&status) != -1)
+            {
+                // true if child terminated normally and had an exit status of 0 (success)
+                ret = WIFEXITED(status) && (WEXITSTATUS(status) == 0);
+            }
+            else
+            {
+                ret = false;
+            }
+
+        }
+    }
 
     va_end(args);
 
-    return true;
+    return ret;
 }
+
